59da45299a7e8fbd7ba8b806e3338d5b771ee1f7
DoChuan13 <chuando.9213@gmail.com>
2023-03-16 06:48:03 +0700

    "update"

--- src/myList/MyList.java
+++ src/myList/MyList.java
@@ -12,11 +12,13 @@ public class MyList<E> {
     }
 
     private void ensureCapa() {
+        //Method tăng gấp đôi kích thước mảng và sao chép mảng
         int newSize = elements.length * 2;
         elements = Arrays.copyOf(elements, newSize);
     }
 
     public void add(E e) {
+        //Kiểm tra kích thước mảng có thỏa mãn kích thước thực?? => nhân đôi kích thước (nếu cần), thêm phần tử và tăng bộ đếm kích thước mảng thực
         if (size == elements.length) {
             ensureCapa();
         }
@@ -24,6 +26,7 @@ public class MyList<E> {
     }
 
     public E get(int i) {
+        //tìm kiếm phần tử thứ i trong mảng và trả ra
         if (i >= size || i < 0) {
             throw new IndexOutOfBoundsException();
         } else {
